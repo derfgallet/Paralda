53c5a64d49ae46281e20e4d0e057e7b37ebcc75c
derfgallet <derf.gallet@free.fr>
2017-05-20 17:52:00 +0200

    Fourth Talk and counting ...

--- app/GPIO.js
+++ app/GPIO.js
@@ -11,18 +11,24 @@ module.exports = {
 var rpio = require('rpio');
 // Engine Relay on GPIO 23 -> Pin 16
 var _EngineGPIOPin=16;
+var _Relay2Pin=18;
+var _Relay3Pin=22;
+var _Relay4Pin=32;
 
 function _GPIOInit()
 {
     rpio.open(_EngineGPIOPin,rpio.OUTPUT,rpio.HIGH);
+    rpio.open(_Relay2Pin,rpio.OUTPUT,rpio.HIGH);
+    rpio.open(_Relay3Pin,rpio.OUTPUT,rpio.HIGH);
+    rpio.open(_Relay4Pin,rpio.OUTPUT,rpio.HIGH);
 }
 
 function _StopEngine()
 {
-    rpio.write(_EngineGPIOPin,rpio.HIGH);
+    rpio.write(_EngineGPIOPin,rpio.LOW);
 }
 
 function _EnableEngine()
 {
-    rpio.write(_EngineGPIOPin,rpio.LOW);
+    rpio.write(_EngineGPIOPin,rpio.HIGH);
 }
